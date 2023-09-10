ca19090015f993c249e6cb7e158f4dfe036ec283
Ada <ada@gnous.eu>
2023-09-10 16:16:19 +0200

    change(backend: redirect): return 404 when link not found

--- ls/redirect.py
+++ ls/redirect.py
@@ -1,19 +1,27 @@
 """Shortener itself."""
-from flask import Blueprint, Response, redirect
 
-from .db import get_db
+from flask import Blueprint, redirect, render_template
+from werkzeug import Response
+
+from ls.config import get_shortener_domain
+from ls.db import get_db
+from ls.utils import verify_link_exist
 
 redirect_url = Blueprint("redirect", __name__)
 
+shortener_domain = get_shortener_domain()
 
-@redirect_url.route("/", defaults={"path": ""}, host="localhost")
-@redirect_url.route("/<path:path>", host="localhost")
-def make_redirect(path: str) -> Response:
+
+@redirect_url.route("/<path:path>", host=shortener_domain)
+def make_redirect(path: str) -> Response | tuple[str, int]:
     """
 
     :param path: The fetched path
     :return: The redirection
     """
-    db = get_db()
-    query = db.execute("SELECT real_url from link WHERE url=?", (path,))
-    return redirect(query.fetchall()[0][0])
+    if verify_link_exist(path):
+        db = get_db()
+        query = db.execute("SELECT real_url from link WHERE url=?", (path,))
+        return redirect(query.fetchall()[0][0])
+
+    return render_template("404.html.j2"), 404
